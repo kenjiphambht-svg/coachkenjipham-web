02663d6f49ed126d514573da9063ab4ed44b34f5
kenjiphambht-svg <kenjipham.bht@gmail.com>
2026-09-01 18:54:35 +0700

    [p04-brand-visual-run] P03 assemble final high-res Hero source

---
+++ src/components/homepage/generated/heroScene01Final/index.ts
@@ -0,0 +1,13 @@
+import chunk01 from "./chunk01";
+import chunk02 from "./chunk02";
+import chunk03 from "./chunk03";
+import chunk04 from "./chunk04";
+import chunk05 from "./chunk05";
+import chunk06 from "./chunk06";
+import chunk07 from "./chunk07";
+import chunk08 from "./chunk08";
+import chunk09 from "./chunk09";
+
+const heroScene01Final = `data:image/webp;base64,${chunk01}${chunk02}${chunk03}${chunk04}${chunk05}${chunk06}${chunk07}${chunk08}${chunk09}`;
+
+export default heroScene01Final;
